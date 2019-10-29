958b3e0f3a0b4160e51e3d5bccc13514c686202c
slpop <staceypopenfoose@gmail.com>
2019-10-29 11:32:57 -0400

    php files

---
+++ header.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Verify</title>
+
+  <!-- Custom fonts for this template-->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+
+  <!-- Page level plugin CSS-->
+  <link href="vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
+
+  <!-- Custom styles for this template-->
+  <link href="css/sb-admin.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+  <nav class="navbar navbar-expand navbar-dark bg-dark static-top">
+
+    <a class="navbar-brand mr-1" href="index.php">Asset Manager</a>
+
+    <button class="btn btn-link btn-sm text-white order-1 order-sm-0" id="sidebarToggle" href="#">
+      <i class="fas fa-bars"></i>
+    </button>
+
+  </nav>
+
+  <div id="wrapper">
+
+    <!-- Sidebar -->
+    <ul class="sidebar navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">
+          <span>Verify</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="addasset.php">
+        <span>Add Assets</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="charts.php">
+          <span>Reports</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="tables.php">
+          <span>Update Options</span></a>
+      </li>
+    </ul>
