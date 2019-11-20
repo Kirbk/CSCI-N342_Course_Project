4b4e2cd78e59f8f2b97703a11b17de77071a9f32
shead1234 <57144450+shead1234@users.noreply.github.com>
2019-11-20 13:56:42 -0500

    Update addasset.php

--- addasset.php
+++ addasset.php
@@ -3,13 +3,9 @@
 	if ($_SESSION['aid'] == NULL)
 		Header("Location:login.php");
 	include 'header.php';
-
 	require_once "config.php";
-
-
 	if (isset($_POST['submit'])) {
 		echo "<a style='color:white'>Successfully added!</a><br />";
-
 		$stmt = $con->prepare("insert into Proj_DEVICE (SerialNum, Category, Manufacturer, ModelNum, Location, User, Network, PurchaseDate, WarrantyDate, LastChecked, Surplus, Notes) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
 		$stmt->execute(array($_POST['SerialNum'], $_POST['Category'], $_POST['Manufacturer'], $_POST['model'], $_POST['Location'], $_POST['User'], 1, $_POST['PurchaseDate'], 0, $_POST['PurchaseDate'], 0, $_POST['notes']));
 	}
@@ -39,11 +35,9 @@
 				<?php
 					$stmt = $con->prepare("select CID as cid, Name as name from Proj_CATEGORY");
 					$stmt->execute();
-
 					while ($row = $stmt->fetch(PDO::FETCH_OBJ)) {
 						$id = $row->cid;
 						$name = $row->name;
-
 						echo "<option value='" . $id . "'>" . $name . "</option>";
 					}
 				?>
@@ -60,11 +54,9 @@
 				<?php
 					$stmt = $con->prepare("select MID as cid, Name as name from Proj_MANUFACTURER");
 					$stmt->execute();
-
 					while ($row = $stmt->fetch(PDO::FETCH_OBJ)) {
 						$id = $row->cid;
 						$name = $row->name;
-
 						echo "<option value='" . $id . "'>" . $name . "</option>";
 					}
 				?>
@@ -95,43 +87,41 @@
       <input type="date" name="PurchaseDate" id="PurchaseDate" name="PurchaseDate">
 		</div>
 	</div>
-
-	<div class="col-md-4 mb-3">
+<div class="form-row">
+	<div class="col-md-3 mb-3">
 	<label for="User"><font color="white">User</font></label>
 	<select class="custom-select mr-sm-2" id="User" name="User" required>
 	 <option selected>Choose...</option>
 	 <?php
 		 $stmt = $con->prepare("select UID as uid, FirstName as first, LastName as last from Proj_USER");
 		 $stmt->execute();
-
 		 while ($row = $stmt->fetch(PDO::FETCH_OBJ)) {
 			 $id = $row->uid;
 			 $first = $row->first;
 			 $last = $row->last;
-
 			 echo "<option value='" . $id . "'>" . $first . " " . $last . "</option>";
 		 }
 	 ?>
  </select>
 			</div>
 
-	     <div class="col-md-4 mb-3">
+	     <div class="col-md-3 mb-3">
 		   <label for="Location"><font color="white">Location</font></label>
 		   <select class="custom-select mr-sm-2" id="Location" name="Location" required>
 				<option selected>Choose...</option>
 				<?php
 					$stmt = $con->prepare("select LID as lid, Name as name from Proj_LOCATION");
 					$stmt->execute();
-
 					while ($row = $stmt->fetch(PDO::FETCH_OBJ)) {
 						$id = $row->lid;
 						$name = $row->name;
-
 						echo "<option value='" . $id . "'>" . $name . "</option>";
 					}
 				?>
 			</select>
 		 	     </div>
+				 </div>
+				 <div class="form-row">
 					<div class="col-md-3 mb-3">
 					 <div class="form-row">
 				<label for="Warranty"><font color="white">Warranty Date</font></label>
@@ -140,13 +130,32 @@
 			 </div>
 		 </div>
 	 </div>
-				<div class="form-row">
+	 <div class="col-md-3 mb-3">
+	 <label for="Network"><font color="white">Network</font></label>
+	 <select class="custom-select mr-sm-2" id="Network" name="Network" required>
+	  <option selected>Choose...</option>
+	  <?php
+	 	 $stmt = $con->prepare("select NID as nid, Name as name from Proj_NETWORK");
+	 	 $stmt->execute();
+	 	 while ($row = $stmt->fetch(PDO::FETCH_OBJ)) {
+	 		 $id = $row->nid;
+	 		 $name = $row->name;
+	 		 echo "<option value='" . $id . "'>" . $name . "</option>";
+	 	 }
+	  ?>
+	 </select>
+ </div>
+</div>
+		<div class="col-md-3 mb-3">
 				 <div class="form-group">
     <label for="notes"><font color="white">Additional notes</font></label>
     <textarea class="form-control" id="notes" name="notes" rows="4"></textarea>
   	</div>
-	</div>
 </div>
+	<div class="form-check">
+    <input type="checkbox" class="form-check-input" id="Check">
+    <label class="form-check-label" for="Check"><font color="white">Surplus</font></label>
+  </div>
 	<input type='submit' id='submit' name="submit" />
 </form>
 
